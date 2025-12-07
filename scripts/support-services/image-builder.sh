dc4aff039d39c14dd86c85b5f3c3b1e18cdf3d7a
Mohamed El Gindi <mo.gindi@gmail.com>
2025-12-07 21:36:15 +0100

    fix removing temp files as final stage

--- scripts/support-services/image-builder.sh
+++ scripts/support-services/image-builder.sh
@@ -173,9 +173,35 @@ cat > win2022-gui.json <<'EOT'
             "type": "powershell",
             "scripts": ["scripts/cleanup.ps1"],
             "pause_before": "1m"
+        },
+        {
+            "type": "powershell",
+            "inline": ["Get-ChildItem \"C:\\Windows\\Temp\\*\" -Recurse -Force -Verbose -ErrorAction SilentlyContinue | Remove-Item -Force -Verbose -Recurse -ErrorAction SilentlyContinue"]
         }
     ]
 }
+EOT
+
+cat > ./scripts/cleanup.ps1 <<'EOT'
+Function Cleanup {
+
+    Clear-Host
+
+    ## Stops the windows update service.
+    Get-Service -Name wuauserv | Stop-Service -Force -Verbose -ErrorAction SilentlyContinue
+
+    ## Deletes the contents of windows software distribution.
+    Get-ChildItem "C:\Windows\SoftwareDistribution\*" -Recurse -Force -Verbose -ErrorAction SilentlyContinue | Remove-Item -Force -Verbose -Recurse -ErrorAction SilentlyContinue
+
+    ## Delets all files and folders in user's Temp folder.
+    Get-ChildItem "C:\users\*\AppData\Local\Temp\*" -Recurse -Force -ErrorAction SilentlyContinue | Remove-Item -Force -Verbose -Recurse -ErrorAction SilentlyContinue
+
+    ## Remove all files and folders in user's Temporary Internet Files.
+    Get-ChildItem "C:\users\*\AppData\Local\Microsoft\Windows\Temporary Internet Files\*" -Recurse -Force -Verbose -ErrorAction SilentlyContinue | Remove-Item -Force -Recurse -ErrorAction SilentlyContinue
+
+}
+
+Cleanup
 EOT
 
 # plugin config
